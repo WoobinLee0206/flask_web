b042c6afe6e62b079a4ea5a2043117f821ec94ab
Woobin Lee <ggompang0206@gmail.com>
2020-07-20 17:13:07 +0900

    list for step 1

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from data import Articles
 
 app = Flask(__name__)
 app.debug = True
@@ -19,7 +20,9 @@ def about():
 def articles():
     print("Success")
     # return "TEST"
-    return render_template('articles.html', hello = "GaryKim")
+    articles = Articles()
+    print(len(articles))
+    return render_template('articles.html', a = articles)
 
 if __name__ == '__main__' :
     # app.run(host = '0.0.0.0', port = '8080')
