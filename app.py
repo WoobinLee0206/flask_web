e7ac721c59734a6e2b8d46f04f9f81cf00e62685
Woobin Lee <ggompang0206@gmail.com>
2020-07-21 12:37:36 +0900

    list for step 1

--- app.py
+++ app.py
@@ -1,29 +1,40 @@
-from flask import Flask, render_template
+from flask import Flask ,render_template
 from data import Articles
-
 app = Flask(__name__)
-app.debug = True
+app.debug=True
 
 @app.route('/')
 def index():
     print("Success")
     # return "TEST"
-    return render_template('home.html', hello = "GaryKim") #render_template : html 형식 파일의 데이터를 텍스트 형식으로 바꿔서 날려줌
+    return render_template('home.html',hello="GaryKim")
 
 @app.route('/about')
 def about():
     print("Success")
     # return "TEST"
-    return render_template('about.html', hello = "GaryKim")
+    return render_template('about.html',hello="GaryKim")
 
-@app.route('/articles', methods = ['GET', 'POST'])
+@app.route('/articles', methods=['GET','POST'])
 def articles():
     print("Success")
     # return "TEST"
     articles = Articles()
     print(len(articles))
-    return render_template('articles.html', articles = articles)
+    return render_template('articles.html',articles=articles)
 
-if __name__ == '__main__' :
-    # app.run(host = '0.0.0.0', port = '8080')
+@app.route('/test')
+def show_image():
+    return render_template('image.html')
+
+@app.route('/article/<int:id>')
+def article(id):
+    print(id)
+    articles= Articles()[id-1]
+    print(articles)
+    return render_template('article.html',data = articles)
+    # return "Success"
+
+if __name__ =='__main__':
+    # app.run(host='0.0.0.0', port='8080')
     app.run()
---
+++ test.py
@@ -0,0 +1,11 @@
+print("now Start")
+
+def hello():
+    print("Say hello")
+
+def bye():
+    print("See U")
+
+if __name__ == '__main__':
+    hello()
+    bye()
